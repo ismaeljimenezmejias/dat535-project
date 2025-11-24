6e81c65ab1eaba1bd518a285d58b16da8d4ccadd
ismaeljimenezmejias <isjim2326@uis.no>
2025-11-24 19:41:12 +0100

    query corrected

--- serving.py
+++ serving.py
@@ -9,7 +9,9 @@ spark = SparkSession.builder.appName("UseCase1_RDD").getOrCreate()
 df = spark.read.parquet("gs://medallion-dat535/gold/rdd/mental_health_clean.parquet")
 
 # Convertimos el DF a RDD y cacheamos
+
 rdd = df.rdd.repartition(4).cache()
+rdd = rdd.sample(False, 0.1, seed=42)
 
 print("\n========== USE CASE 1 (RDD ONLY) ==========\n")
 start = time.time()
@@ -35,7 +37,7 @@ for col in categorical_cols:
     # (category_value, isHighStress)
     pairs = rdd.map(lambda row: (
         row[col],
-        1 if row["IncreasingStress"].strip().lower() == "Yes" else 0
+        1 if row["IncreasingStress"].strip().lower() == "yes" else 0
     )).filter(lambda x: x[0] is not None)  # eliminamos nulos
 
     # (category_value, (sumHigh, count))
